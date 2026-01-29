a2e36b8085121103982d976761b2be6f44e27976
Sarma HK <sarmahk831@gmail.com>
2026-01-29 15:56:19 +0530

    Feature: print addition result

--- pydemo.py
+++ pydemo.py
@@ -4,10 +4,10 @@ def add_numbers(a, b):
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-# def subtract_numbers(a, b):
-#     return a - b
+def subtract_numbers(a, b):
+    return a - b
 
 result = add_numbers(num1, num2)
 result2 =subtract_numbers(num1, num2)
 print("Sum:", result)
-# print("Difference:", result2)
+print("Difference:", result2)
