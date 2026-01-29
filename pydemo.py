84d3536b49777ceba36265611c221d18cd14af95
Sarma HK <sarmahk831@gmail.com>
2026-01-29 15:52:49 +0530

    intial Commit

---
+++ pydemo.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# def subtract_numbers(a, b):
+#     return a - b
+
+result = add_numbers(num1, num2)
+result2 =subtract_numbers(num1, num2)
+print("Sum:", result)
+# print("Difference:", result2)
